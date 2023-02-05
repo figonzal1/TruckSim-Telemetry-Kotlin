7073c64ceb77d95c02f2439589738bd6b9b40f7f
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-02-05 12:06:33 -0300

    Improve docs for Events
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- src/main/kotlin/scs_sdk/model/events/fine/EventsFine.kt
+++ src/main/kotlin/scs_sdk/model/events/fine/EventsFine.kt
@@ -1,5 +1,13 @@
 package scs_sdk.model.events.fine
 
+/**
+ * Class that hold events information related with fines
+ *
+ * @author Felipe Gonzalez
+ * @property offence type
+ * @property amount of the fine
+ * @property active [True|False] if event is still active
+ */
 data class EventsFine(
     val offence: String,
     val amount: Long,
--- src/main/kotlin/scs_sdk/model/events/tollgate/EventsTollgate.kt
+++ src/main/kotlin/scs_sdk/model/events/tollgate/EventsTollgate.kt
@@ -1,5 +1,12 @@
 package scs_sdk.model.events.tollgate
 
+/**
+ * Class that hold events information related to tollgates
+ *
+ * @author Felipe Gonzalez
+ * @property amount of ticket
+ * @property active [True|False] if event is still active
+ */
 data class EventsTollgate(
     val amount: Long,
     val active: Boolean
