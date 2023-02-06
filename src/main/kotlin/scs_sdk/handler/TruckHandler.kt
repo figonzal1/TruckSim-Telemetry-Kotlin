d64f927df2c8865339793aa8b42aeaae36ca0ef9
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-02-06 14:05:51 -0300

    Reorder code in TruckHandler.kt
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- src/main/kotlin/scs_sdk/handler/TruckHandler.kt
+++ src/main/kotlin/scs_sdk/handler/TruckHandler.kt
@@ -20,88 +20,98 @@ import utils.*
 
 fun truck(rawData: ByteArray) = with(rawData) {
     Truck(
-        transmission = Transmission(
-            forwardGears = getUInt(68).toInt(),
-            reverseGears = getUInt(72).toInt(),
-            selectorCount = getUInt(84).toInt(),
-            slotsGear = getUIntArray(512, 32).map { it.toInt() } as ArrayList<Int>,
-            slotsHandlePosition = getUIntArray(184, 32).map { it.toInt() },
-            slotsSelector = getUIntArray(312, 32).map { it.toInt() },
-            selectedGear = getUInt(504).toInt(),
-            dashboardGear = getUInt(508).toInt(),
-            gearRatiosForward = getFloatArray(816, 24),
-            gearRatiosReverse = getFloatArray(912, 8),
-            damage = getFloat(1040),
-            shifterType = getString(3196, 16)
-        ),
-        brakes = Brakes(
-            retarder = Retarder(
-                steps = getUInt(76).toInt(),
-                level = getUInt(108).toInt()
-            ),
-            airPressure = AirPressure(
-                value = getFloat(992),
-                warning = AirPressureAlertWarning(
-                    factor = getFloat(720),
-                    enabled = getBool(1568)
-                ),
-                emergency = AirPressureAlertEmergency(
-                    factor = getFloat(724),
-                    enabled = getBool(1569)
-                )
-            ),
-            temperature = getFloat(996),
-            isParkingBrakeActive = getBool(1566),
-            isMotorBreakActive = getBool(1567)
-        ),
-        lights = Lights(
-            auxFront = getUInt(112).toInt(),
-            auxRoof = getUInt(116).toInt(),
-            dashboardBackLight = getFloat(1032),
-            blinker = Blinker(
-                left = BlinkerStatus(getBool(1578), getBool(1580)),
-                right = BlinkerStatus(getBool(1579), getBool(1581))
-            ),
-            isParkingBrakeOn = getBool(1582),
-            isBeamLowOn = getBool(1583),
-            isBeamHighOn = getBool(1584),
-            isBeaconOn = getBool(1585),
-            isBrakeOn = getBool(1586),
-            isReverseOn = getBool(1587),
-            isHazardOn = getBool(1588)
-        ),
-        liquids = Liquids(
-            fuel = Fuel(
-                capacity = getFloat(704),
-                warning = WarningLevels(getFloat(708), getBool(1570)),
-                value = getFloat(1000),
-                avgConsumption = getFloat(1004),
-                range = getFloat(1008)
-            ),
-            adBlue = AdBlue(
-                capacity = getFloat(712),
-                warning = WarningLevels(getFloat(716), getBool(1571)),
-                value = getFloat(1012)
-            ),
-        ),
-        engine = Engine(
-            oil = Oil(
-                pressure = getFloat(1016),
-                warning = WarningLevels(getFloat(728), getBool(1572)),
-                temperature = getFloat(1020),
-            ),
-            water = Water(
-                temperature = rawData.getFloat(1024),
-                warning = WarningLevels(rawData.getFloat(732), rawData.getBool(1573))
-            ),
-            battery = Battery(
-                value = rawData.getFloat(1028),
-                warning = WarningLevels(rawData.getFloat(736), rawData.getBool(1574))
-            ),
-            rpm = Rpm(rawData.getFloat(740), rawData.getFloat(952)),
-            damage = rawData.getFloat(1036),
-            enabled = rawData.getBool(1576)
-        )
-        //TODO: Implement engine, differential, speed,cruise control, cabin, chassis, odomoeter, electric, wipers, head, hook, acceleration, position, orientation, make,brand,model, plate, damage, liftaxle
+        transmission = transmission(),
+        brakes = brakes(),
+        lights = lights(),
+        liquids = liquids(),
+        engine = engine()
+        //TODO: Implement differential, speed,cruise control, cabin, chassis, odomoeter, electric, wipers, head, hook, acceleration, position, orientation, make,brand,model, plate, damage, liftaxle
     )
 }
+
+private fun ByteArray.transmission() = Transmission(
+    forwardGears = getUInt(68).toInt(),
+    reverseGears = getUInt(72).toInt(),
+    selectorCount = getUInt(84).toInt(),
+    slotsGear = getUIntArray(512, 32).map { it.toInt() } as ArrayList<Int>,
+    slotsHandlePosition = getUIntArray(184, 32).map { it.toInt() },
+    slotsSelector = getUIntArray(312, 32).map { it.toInt() },
+    selectedGear = getUInt(504).toInt(),
+    dashboardGear = getUInt(508).toInt(),
+    gearRatiosForward = getFloatArray(816, 24),
+    gearRatiosReverse = getFloatArray(912, 8),
+    damage = getFloat(1040),
+    shifterType = getString(3196, 16)
+)
+
+private fun ByteArray.brakes() = Brakes(
+    retarder = Retarder(
+        steps = getUInt(76).toInt(),
+        level = getUInt(108).toInt()
+    ),
+    airPressure = AirPressure(
+        value = getFloat(992),
+        warning = AirPressureAlertWarning(
+            factor = getFloat(720),
+            enabled = getBool(1568)
+        ),
+        emergency = AirPressureAlertEmergency(
+            factor = getFloat(724),
+            enabled = getBool(1569)
+        )
+    ),
+    temperature = getFloat(996),
+    isParkingBrakeActive = getBool(1566),
+    isMotorBreakActive = getBool(1567)
+)
+
+private fun ByteArray.lights() = Lights(
+    auxFront = getUInt(112).toInt(),
+    auxRoof = getUInt(116).toInt(),
+    dashboardBackLight = getFloat(1032),
+    blinker = Blinker(
+        left = BlinkerStatus(getBool(1578), getBool(1580)),
+        right = BlinkerStatus(getBool(1579), getBool(1581))
+    ),
+    isParkingBrakeOn = getBool(1582),
+    isBeamLowOn = getBool(1583),
+    isBeamHighOn = getBool(1584),
+    isBeaconOn = getBool(1585),
+    isBrakeOn = getBool(1586),
+    isReverseOn = getBool(1587),
+    isHazardOn = getBool(1588)
+)
+
+private fun ByteArray.liquids() = Liquids(
+    fuel = Fuel(
+        capacity = getFloat(704),
+        warning = WarningLevels(getFloat(708), getBool(1570)),
+        value = getFloat(1000),
+        avgConsumption = getFloat(1004),
+        range = getFloat(1008)
+    ),
+    adBlue = AdBlue(
+        capacity = getFloat(712),
+        warning = WarningLevels(getFloat(716), getBool(1571)),
+        value = getFloat(1012)
+    ),
+)
+
+private fun ByteArray.engine() = Engine(
+    oil = Oil(
+        pressure = getFloat(1016),
+        warning = WarningLevels(getFloat(728), getBool(1572)),
+        temperature = getFloat(1020),
+    ),
+    water = Water(
+        temperature = getFloat(1024),
+        warning = WarningLevels(getFloat(732), getBool(1573))
+    ),
+    battery = Battery(
+        value = getFloat(1028),
+        warning = WarningLevels(getFloat(736), getBool(1574))
+    ),
+    rpm = Rpm(getFloat(740), getFloat(952)),
+    damage = getFloat(1036),
+    enabled = getBool(1576)
+)
