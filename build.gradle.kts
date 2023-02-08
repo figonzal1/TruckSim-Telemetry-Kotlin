3f8fffe5f0b49e5bdc26b6550bb3bb91947488da
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-02-08 12:21:41 -0300

    Copyright profile
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+/*
+ * This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package
+ *
+ *  Author: Felipe González Alarcón
+ *  Email: felipe.gonzalezalarcon94@gmail.com
+ *
+ *  Project: ETS2-Telemetry
+ *  Module: ETS2-Telemetry
+ *  Last modified: 08-02-23 12:18
+ */
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
--- src/main/kotlin/scs_sdk/model/game/Game.kt
+++ src/main/kotlin/scs_sdk/model/game/Game.kt
@@ -1,3 +1,14 @@
+/*
+ * This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package
+ *
+ *  Author: Felipe González Alarcón
+ *  Email: felipe.gonzalezalarcon94@gmail.com
+ *
+ *  Project: ETS2-Telemetry
+ *  Module: ETS2-Telemetry.main
+ *  Last modified: 08-02-23 12:18
+ */
+
 package scs_sdk.model.game
 
 /**
