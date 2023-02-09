6889b25beeec33f37b5e61daa1cc928eade0299b
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-02-09 13:02:53 -0300

    README added & example in Main.kt
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,38 +9,32 @@
  *  Last modified: 09-02-23 01:03
  */
 
-/*
- * This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package
- *
- *  Author: Felipe González Alarcón
- *  Email: felipe.gonzalezalarcon94@gmail.com
- *
- *  Project: TruckSim-Telemetry-Kotlin
- *  Module: TruckSim-Telemetry-Kotlin.main
- *  Last modified: 09-02-23 01:02
- */
-
-
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import mu.KotlinLogging
 import scs_sdk.ScsTelemetry
 
-val logger = KotlinLogging.logger { }
+suspend fun main(args: Array<String>) {
 
-fun main(args: Array<String>) {
+    coroutineScope {
 
-    val telemetry = ScsTelemetry()
+        val telemetry = ScsTelemetry()
 
-    runBlocking {
-
-        //Listen changes
         launch {
-            telemetry.watch()
+            telemetry.watch(delayTime = 2000L)
         }
 
         launch {
+            //Collect only one value
+            telemetry.telemetryFlow.first {
+                println("Telemetry data: $it")
+                true
+            }
 
+            //Collect infinite values
+            telemetry.telemetryFlow.collect {
+                println("Telemetry data: $it")
+            }
         }
     }
 }
